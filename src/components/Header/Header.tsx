268c69f9425a118de9b4593e59abccde40038441
Elena <110896958+Elena-web@users.noreply.github.com>
2025-07-27 13:35:28 +0300

    refactor: сonvert Header to functional component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,21 +6,19 @@ interface HeaderProps {
   onSearch: (term: string) => void;
 }
 
-class Header extends React.Component<HeaderProps> {
-  render() {
-    return (
-      <header className={s.header}>
-        <h1 className={s.title}>Discover Your Perfect Breed</h1>
-        <p className={s.subtitle}>
-          Use the field below to find the cat breed you are interested in.
-        </p>
-        <p className={s.subtitle}>
-          For example: Maine, Bengal, Sphynx, Norwegian, Persian,Ocicat,etc.
-        </p>
-        <Search onSearch={this.props.onSearch} />
-      </header>
-    );
-  }
-}
+const Header: React.FC<HeaderProps> = ({ onSearch }) => {
+  return (
+    <header className={s.header}>
+      <h1 className={s.title}>Discover Your Perfect Breed</h1>
+      <p className={s.subtitle}>
+        Use the field below to find the cat breed you are interested in.
+      </p>
+      <p className={s.subtitle}>
+        For example: Maine, Bengal, Sphynx, Norwegian, Persian, Ocicat, etc.
+      </p>
+      <Search onSearch={onSearch} />
+    </header>
+  );
+};
 
 export default Header;
